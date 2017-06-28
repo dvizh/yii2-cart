d6c4b3c855ba7104ba1cb2efc89757d0bad914ef
Илья Егоров <pistol88@gmail.com>
2017-06-28 15:22:41 +0500

    change schema

--- src/migrations/m260519_000708_alter_cart_table.php
+++ src/migrations/m260519_000708_alter_cart_table.php
@@ -6,7 +6,7 @@ class m260519_000708_alter_cart_table extends Migration
 {
     public function up()
     {
-        $this->addColumn('{{%cart}}', 'tmp_user_id', 'VARCHAR(55) NULL AFTER `user_id`');
+        $this->addColumn('{{%cart}}', 'tmp_user_id', $this->string(55)->null() . ' AFTER `user_id`');
         $this->createIndex('tmp_user_id', '{{%cart}}', 'tmp_user_id');
     }
 
